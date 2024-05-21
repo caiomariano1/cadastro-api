3d99ef0f0825b3cc3d5543a580cbe2abff15e58c
Caio Mariano <caio.siiiilva@gmail.com>
2024-05-21 18:36:18 -0300

    feat: Criar métodos para gerar token Jwt e validar usuário.

---
+++ Data/Identity/AuthenticateService.cs
@@ -0,0 +1,78 @@
+﻿using cadastro_api.Domain.Account;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace cadastro_api.Data.Identity
+{
+    public class AuthenticateService : IAuthenticate
+    {
+        private readonly SistemaContatosDbContext _context;
+        private readonly IConfiguration _configuration;
+
+        public AuthenticateService(SistemaContatosDbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        public async Task<bool> AuthenticateAsync(string email, string senha)
+        {
+            var usuario = await _context.Usuario.Where(x => x.Email.ToLower() == email.ToLower()).FirstOrDefaultAsync();
+            if (usuario == null) 
+            {
+                return false;
+            }
+
+            using var hmac = new HMACSHA512(usuario.PasswordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(senha));
+            for (int x = 0; x < computedHash.Length; x++) 
+            {
+                if (computedHash[x] != usuario.PasswordHash[x]) return false;
+            }
+
+            return true;
+        }
+
+        public string GenerateToken(int id, string email)
+        {
+            var claims = new[]
+            {
+                new Claim("id", id.ToString()),
+                new Claim("email", email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var privateKey = new SymmetricSecurityKey(Encoding.UTF8.
+                GetBytes(_configuration["Jwt:SecretKey"]));
+
+            var credentials = new SigningCredentials(privateKey, SecurityAlgorithms.HmacSha256);
+
+            var expiration = DateTime.UtcNow.AddMinutes(60);
+
+            JwtSecurityToken token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: expiration,
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public async Task<bool> UserExist(string email)
+        {
+            var usuario = await _context.Usuario.Where(x => x.Email.ToLower() == email.ToLower()).FirstOrDefaultAsync();
+            if (usuario == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Domain/Account/IAuthenticate.cs
@@ -0,0 +1,9 @@
+﻿namespace cadastro_api.Domain.Account
+{
+    public interface IAuthenticate
+    {
+        Task<bool> AuthenticateAsync(string email, string senha);
+        Task<bool> UserExist(string email);
+        public string GenerateToken(int id, string email);
+    }
+}
